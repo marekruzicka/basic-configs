7bd313b2badf394418c43c278f7c75cb12ae0eba
Marek Ruzicka <marek.ruzicka@t-systems.sk>
2015-08-20 13:20:53 +0200

    renaming env file

--- display.sh
+++ display.sh
@@ -1,2 +1,3 @@
-xrandr --output DP1 --mode 1920x1080 --left-of eDP1 --auto --primary
-xrandr --output eDP1 --mode 1366x768
+xrandr --output DP1 --mode 1920x1080 --auto --primary
+xrandr --output eDP1 --mode 1366x768 --left-of DP1
+xrandr --output HDMI2 --mode 1920x1080 --right-of DP1
